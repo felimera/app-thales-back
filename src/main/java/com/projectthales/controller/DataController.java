7347f7591ad0e4272d19c3236d8419692916b7fd
andres.mera <feli041089@gmail.com>
2025-03-12 16:41:47 -0500

    code 1 : Creating the controller with the app endpoints.

---
+++ src/main/java/com/projectthales/controller/DataController.java
@@ -0,0 +1,33 @@
+package com.projectthales.controller;
+
+import com.projectthales.model.dto.DataDto;
+import com.projectthales.service.IDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/v1/data")
+public class DataController {
+    private IDataService dataService;
+
+    @Autowired
+    public DataController(IDataService dataService) {
+        this.dataService = dataService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<DataDto>> getDataList() {
+        return ResponseEntity.ok(dataService.getDataList());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<DataDto> get(@PathVariable(name = "id") Integer id) {
+        return ResponseEntity.ok(dataService.getById(id));
+    }
+}
