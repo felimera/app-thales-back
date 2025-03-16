8888d40a4eaf46217894d6c67d467b16d72285d4
andres.mera <feli041089@gmail.com>
2025-03-16 11:18:40 -0500

    code 5 : Document the controller.

--- src/main/java/com/projectthales/controller/EmployeeController.java
+++ src/main/java/com/projectthales/controller/EmployeeController.java
@@ -2,6 +2,9 @@ package com.projectthales.controller;
 
 import com.projectthales.model.dto.EmployeeDto;
 import com.projectthales.service.IEmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "Employee", description = "Operations related to employees.")
 @RestController
 @RequestMapping(path = "/api/v1/employee")
 public class EmployeeController {
@@ -21,13 +25,15 @@ public class EmployeeController {
         this.dataService = dataService;
     }
 
+    @Operation(summary = "Gets a list of employees.")
     @GetMapping()
     public ResponseEntity<List<EmployeeDto>> getDataList() {
         return ResponseEntity.ok(dataService.getEmployeeList());
     }
 
+    @Operation(summary = "Get an employee by their ID.")
     @GetMapping(path = "/{id}")
-    public ResponseEntity<EmployeeDto> getById(@PathVariable(name = "id") Integer id) {
+    public ResponseEntity<EmployeeDto> getById(@Parameter(description = "Employee ID to search for.") @PathVariable(name = "id") Integer id) {
         return ResponseEntity.ok(dataService.getById(id));
     }
 }
